9f8db6921ebbc6616a00d21e47fadfec394f010b
Poonati <MPoonati@lynk.lynksystems.com>
2017-10-17 00:28:23 -0500

    Components ng g c customer -d ng g c customer ng g c customer2 -ve Emulated -cd OnPush -d ng g c customer2 -ve Emulated -cd OnPush

--- ngcli-demo/src/app/app.module.ts
+++ ngcli-demo/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { Customer2Component } from './customer2/customer2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    Customer2Component
   ],
   imports: [
     BrowserModule
