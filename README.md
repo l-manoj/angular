f5cff9c0bd150d02bbd65cc1099f4ca03b0eb1e4
Poonati <MPoonati@lynk.lynksystems.com>
2017-10-17 00:41:08 -0500

    Directive

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # ng-cli-demo
 This is a sample application that is created to build, test and manage an angular app using Angular CLI.
+
 Components:
 ng g c customer -d
 ng g c customer
 ng g c customer2 -ve Emulated -cd OnPush -d
 ng g c customer2 -ve Emulated -cd OnPush
+
+Directives:
+ng g d search-box -d
+ng g d search-box
+ng g d search-box2 --flat false -d
+ng g d search-box2 --flat false
 
--- ngcli-demo/src/app/app.module.ts
+++ ngcli-demo/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { Customer2Component } from './customer2/customer2.component';
+import { SearchBoxDirective } from './search-box.directive';
+import { SearchBox2Directive } from './search-box2/search-box2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
-    Customer2Component
+    Customer2Component,
+    SearchBoxDirective,
+    SearchBox2Directive
   ],
   imports: [
     BrowserModule
