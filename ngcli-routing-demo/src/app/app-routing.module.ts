592b1ad1d12bb242a1476a04677ef94512c04f93
Poonati <MPoonati@lynk.lynksystems.com>
2017-10-17 22:24:50 -0500

    Added new components and established routing

--- ngcli-routing-demo/src/app/app-routing.module.ts
+++ ngcli-routing-demo/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomersComponent } from './customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'dashboard'},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'customers',component:CustomersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ngcli-routing-demo/src/app/app.module.ts
+++ ngcli-routing-demo/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
